5c7d5c63b6765a05336aed8c2c01cf7711425bfe
datmicw <dat.micf@gmail.com>
2026-04-28 17:36:52 +0700

    create model for game

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -11,13 +11,14 @@ public class PlayerInput : MonoBehaviour
         if (_player == null || _player.IsMoving)
             return;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // input on pc
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
             _player.Move(Vector3.forward);
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             _player.Move(Vector3.back);
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             _player.Move(Vector3.left);
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             _player.Move(Vector3.right);
     }
 }
